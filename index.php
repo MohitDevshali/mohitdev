db5d04434a1aa97de4bf96b9677ee27e27919982
mohit devshali <39033446+mohitdsevshali@users.noreply.github.com>
2022-07-23 21:36:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,7 @@
 echo "<p align='center'> <font color=blue  size='6pt'>Application is successfully deployed from Git and Github </font> </p>";
 echo "<p align='center'> <font color=black  size='4pt'> Mohit Devshali Resume</font> </p>"
 ?>
-<?php
-
-echo "<iframe src=\"MOHITD.pdf\" width="100%" height="100%"></iframe>";
-
-?>
+<iframe src="MOHITD.pdf" style="width: 100%; height: 100vh"></iframe>
 
 
 </body>
