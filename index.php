c9edc0281314c1689034b64a7d789d781d8db9e5
mohitdsevshali <itswaymohit@gmail.com>
2022-07-23 19:02:00 +0530

    third version

--- index.php
+++ index.php
@@ -9,14 +9,9 @@ echo "<p align='center'> <font color=black  size='4pt'> -by Mohit Devshali</font
 <?php 
 $image_url='download.png';
 ?>
-<<<<<<< HEAD
-
-<img align="center" src="<?php echo $image_url;?>">
-
-=======
 <div style="text-align: center">
 <img align="middle" src="<?php echo $image_url;?>">
  </div>
->>>>>>> 6745aec2ae62ae092b2a732e5a3fcabcfbe71c62
+
 </body>
 </html>
