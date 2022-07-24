89c276307d6d766337cbc1e637297858adeae727
mohit devshali <39033446+mohitdsevshali@users.noreply.github.com>
2022-07-25 00:10:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,11 +14,10 @@ img {
 "I am currently working as a Middleware administator for project Bayer in capgemini, in which I have learnt about 
 middleware components and services. I also have deployement through jenkins for aws servers with the help of docker build number"
 </p>
-<?php
-echo "<p align='center'> <font color=blue  size='6pt'>Application is successfully deployed from Git and Github </font> </p>";
+<p>Application is successfully deployed from Git and Github </font> </p>
 
-echo "<p align='center'> <font color=black  size='4pt'> Mohit Devshali Resume</font> </p>"
-?>
+<p align='center' font color=black  size='4pt'> Mohit Devshali Resume</font> </p>
+
 <iframe src="MOHITD.pdf" style="width: 100%; height: 100vh"></iframe>
 
 
