b9833435da2825d35de49ac3703948f9159c1f4a
mohit devshali <39033446+mohitdsevshali@users.noreply.github.com>
2022-07-24 23:59:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html>
+<head>
+<style>
+img {
+  float: right;
+  border: 1px dotted black;
+  margin: 0px 0px 15px 20px;
+}
+</style>
+</head>
 <body>
 
 <?php
 echo "<p align='center'> <font color=blue  size='6pt'>Application is successfully deployed from Git and Github </font> </p>";
-
-<img src="mohit.png" style="width: 20%; height: 20%" />;
-
+<p><img src="mohit.png" width="100" height="140">
+I am currently working as a Middleware administator for project Bayer in capgemini, in which I have learnt about 
+middleware components and services. I also have deployement through jenkins for aws servers with the help of docker build number
+</p>
 echo "<p align='center'> <font color=black  size='4pt'> Mohit Devshali Resume</font> </p>"
 ?>
 <iframe src="MOHITD.pdf" style="width: 100%; height: 100vh"></iframe>
