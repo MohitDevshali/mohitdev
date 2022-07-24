2b449a1366adb6441973c595132f33c6821a466c
mohit devshali <39033446+mohitdsevshali@users.noreply.github.com>
2022-07-25 00:17:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,11 @@
 <html>
 <head>
 <style>
-img {
-  float: left;
-  border: 1px dotted black;
-  margin: 0px 0px 15px 20px;
-}
+
 </style>
 </head>
 <body>
-<img src="mohit.png" width="20%" height="20%">"I am currently working as a Middleware administator for project Bayer in capgemini, in which I have learnt about 
+<img src="mohit.png" width="20%" height="20%" align="right">"I am currently working as a Middleware administator for project Bayer in capgemini, in which I have learnt about 
 middleware components and services. I also have deployement through jenkins for aws servers with the help of docker build number"
    
 <p>Application is successfully deployed from Git and Github</p>
