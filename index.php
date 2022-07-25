982eb3251435df1797e61b98a9222a47a1bf271c
mohit devshali <39033446+mohitdsevshali@users.noreply.github.com>
2022-07-25 20:51:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,9 @@
     <i> <b>"I am currently working as a Middleware administrator for project Bayer in Capgemini since May 2021, In which I have learnt about 
 middleware components and services and have done deployement through jenkins for aws servers with the help of docker build number.
      I am good in Azure cloud and have done certiciation in Azure i.e Az-900, Az-204, Az-400"</b></i><br>
+  <p><a href="https://github.com/mohitdsevshali/mohitdev">Visit Mohit's GitHub Repository!</a></p>
+  <p><a href="https://www.linkedin.com/in/mohit-devshali-96169a163/">Visit Mohit's LinkedIn Profile!</a></p>
+  
     <br>
     <br>
     <br>
