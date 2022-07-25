5a38c9b4a13c67fb34f2c3a79e53ba69c1f4bf6a
mohit devshali <39033446+mohitdsevshali@users.noreply.github.com>
2022-07-25 20:54:56 +0530

    fifth version

--- index.php
+++ index.php
@@ -10,16 +10,11 @@
  <p>
     <br>
     <br>
-    <br>
-    <br>
     <i> <b>"I am currently working as a Middleware administrator for project Bayer in Capgemini since May 2021, In which I have learnt about 
 middleware components and services and have done deployement through jenkins for aws servers with the help of docker build number.
      I am good in Azure cloud and have done certiciation in Azure i.e Az-900, Az-204, Az-400"</b></i><br>
   <p><a href="https://github.com/mohitdsevshali/mohitdev" target="_blank">Visit Mohit's GitHub Repository!</a></p>
   <p><a href="https://www.linkedin.com/in/mohit-devshali-96169a163/" target="_blank">Visit Mohit's LinkedIn Profile!</a></p>
-  
-    <br>
-    <br>
     <br>
    <br>
    </p>
